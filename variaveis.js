313504cc035ef475db825890e22c04090bbfc17f
The-Albatrosx <isabellam2403@gmail.com>
2025-03-11 10:12:44 -0300

    Feat: Trabalhando com variaveis

---
+++ variaveis.js
@@ -0,0 +1,25 @@
+var idade = 20;
+let nome = "Isabella";
+let ativo = true;
+const PI = 3.14;
+//String para numero 
+
+let num= Number ("81")
+let float = parseFloat("6.6")
+let inteiro =parseInt("10.5")
+
+//numero pra string
+
+let str = String(100)
+let outraStr = toString(42)
+
+console.log(idade)
+console.log(nome)
+console.log(ativo)
+console.log(PI)
+
+if(true){
+    console.log(idade)
+
+   
+}
