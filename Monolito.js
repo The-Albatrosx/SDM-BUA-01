ea1e6c28f228096cb0171b4eb12cf6d8c94ec2fa
The-Albatrosx <822230944@ulife.com.br>
2025-04-01 11:38:58 -0300

    Atualizando o servidor

--- Monolito.js
+++ Monolito.js
@@ -15,9 +15,25 @@ app.post('/users',(req, res)=>{
 })
 
 app.get('/user_data', (req,res)=> {
-
+res.send({users})
 });
 
+app.post('/orders',(req, res)=>{
+    const order = req.body;
+    pedidos.push(order);
+    res.send({messsage: 'Pedido cadastrado com sucesso!',order})
+})
+
+app.get('/orders_data', (req,res)=> {
+res.send({pedidos})
+});
+
+app.get('/all_data',(req, res)=>{
+    res.send({users, pedidos});
+})
+
+
+
 app.listen(3000,()=>{
-    console.log('Servidor monolítico rodando na porta 300');
+    console.log('Servidor monolítico rodando na porta 3000');
 })
